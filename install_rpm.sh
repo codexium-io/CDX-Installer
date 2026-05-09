9ec319ba8aba48d26305856e2025cbc4864cc107
Eric Ostrom <eric@ostromscience.com>
2026-05-09 17:09:03 -0600

    2026.05.09: install_rpm.sh perm on license key set to 400

--- install_rpm.sh
+++ install_rpm.sh
@@ -200,7 +200,7 @@ else
       #echo "cp $LicenseKey /www/CODEXium-License.key"
       #echo ""
       cp $LicenseKey /www/CODEXium-License.key
-      chmod 664 /www/CODEXium-License.key
+      chmod 400 /www/CODEXium-License.key
       chown apache:apache /www/CODEXium-License.key
       if [[ -f /www/CODEXium-License.key ]]
       then
